6c49c1ca7b77d5dea56e208e6307de1e315d5554
Arishba-Khan <cyberfre4k.com@gmail.com>
2024-02-21 02:02:14 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+//store name in a variable
+var Name = "Arishba Khan";
+//print message
+console.log("Good Morning ".concat(Name, "! ,Have a great day in Governor House"));
---
+++ main.ts
@@ -0,0 +1,4 @@
+//store name in a variable
+let Name: string = "Arishba Khan";
+//print message
+console.log(`Good Morning ${Name}! ,Have a great day in Governor House`);
