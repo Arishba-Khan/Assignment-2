d259a39f3dec9e62ea53350d8e8b22a923cc3a95
Arishba-Khan <cyberfre4k.com@gmail.com>
2024-02-25 01:39:05 +0500

    Initial commit

---
+++ Assignment -14/main.js
@@ -0,0 +1,4 @@
+// Array of people to invite to dinner
+var guestList = ["Rukhsar", "Hashir", "Harmain"];
+// Print invitation message
+guestList.forEach(function (person) { return console.log("Dear ".concat(person, ",\n You are invited to a dinner at my place.")); });
---
+++ Assignment -14/main.ts
@@ -0,0 +1,5 @@
+// Array of people to invite to dinner
+const guestList: string[] = ["Rukhsar", "Hashir", "Harmain"];
+
+// Print invitation message
+guestList.forEach(person => console.log(`Dear ${person},\n You are invited to a dinner at my home.`));
