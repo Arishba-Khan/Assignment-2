75ec0a176aac5efd14a519fdbb9571ba83ed8eb3
Arishba-Khan <cyberfre4k.com@gmail.com>
2024-02-25 01:18:50 +0500

    Initial commit

---
+++ Assignment -13/main.js
@@ -0,0 +1,4 @@
+// Array of modes of transportation
+var transportation = ["Rivo", "Audi", "Heavy Bike"];
+// Print statement about each mode of transportation
+transportation.forEach(function (mode) { return console.log("I wanna buy ".concat(mode, ".")); });
---
+++ Assignment -13/main.ts
@@ -0,0 +1,5 @@
+// Array of modes of transportation
+const transportation: string[] = ["Rivo", "Audi", "Heavy Bike"];
+
+// Print statement about each mode of transportation
+transportation.forEach(mode => console.log(`I wanna buy ${mode}.`));
