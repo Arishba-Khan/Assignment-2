7ca781c5a2a03d0f4b3347b0ba1a40200f7f835b
Arishba-Khan <cyberfre4k.com@gmail.com>
2024-02-25 01:08:32 +0500

    Initial commit

---
+++ Assignment -12/main.js
@@ -0,0 +1,6 @@
+var names = ["Dania", "Nialish", "Dua", "Mehak,"];
+var message = ", Hey buddy! How are you today?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
---
+++ Assignment -12/main.ts
@@ -0,0 +1,6 @@
+let names: string [] = ["Dania" , "Nialish" , "Dua" , "Mehak,"];
+let message: string = "Hey buddy! How are you today?"
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
