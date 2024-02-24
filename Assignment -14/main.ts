54a46472a65850301701fe08403ee37301914bca
Arishba-Khan <cyberfre4k.com@gmail.com>
2024-02-25 01:49:09 +0500

    Third commit

--- Assignment -14/main.ts
+++ Assignment -14/main.ts
@@ -1,5 +1,5 @@
 // Array of people to invite to dinner
-const guestList: string[] = ["Rukhsar", "Hashir", "Harmain"];
+const GuestList: string[] = ["Rukhsar", "Hashir", "Harmain"];
 
 // Print invitation message
-guestList.forEach(person => console.log(`Assalam-o-Alikum ${person},\n You are invited to a dinner at my home.`));
+GuestList.forEach(person => console.log(`Assalam-o-Alikum ${person},\n You are invited to a dinner at my home.`));
