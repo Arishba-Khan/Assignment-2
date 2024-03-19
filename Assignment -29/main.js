11ae912fe8ad990f002724af47397c10e8ec62e0
Arishba-khan <Cyberfre4k.com@gmail.com>
2024-03-19 23:49:33 +0500

    Update

---
+++ Assignment -29/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let favorite_fruit = ["Cherry", "Strawberry", "Grapes"];
+if (favorite_fruit.includes("Cherry")) {
+    console.log("You really likes 'Cherry'");
+}
+if (favorite_fruit.includes("Grapes")) {
+    console.log("You really likes 'Grapes'");
+}
---
+++ Assignment -29/main.ts
@@ -0,0 +1,8 @@
+let favorite_fruit = ["Cherry", "Strawberry", "Grapes"];
+
+if (favorite_fruit.includes("Cherry")) {
+    console.log("You really likes 'Cherry'");
+}
+if (favorite_fruit.includes("Grapes")) {
+    console.log("You really likes 'Grapes'");
+}
