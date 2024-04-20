ad03691b2929a8b6c2d37ed221c94dbae700b121
Arishba-Khan <cyberfreak.com@gmail.com>
2024-04-20 22:50:14 +0500

    update

---
+++ CLI Number-guessing-game/app.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var random_number = Math.random();
+console.log(random_number);
---
+++ CLI Number-guessing-game/app.ts
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const random_number = Math.random();
+
+console.log(random_number);
+
+export{}
