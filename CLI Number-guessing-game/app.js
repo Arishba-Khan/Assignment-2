db07df00c258f7d29d4b05fcbe03936a89c46ad5
Arishba-Khan <154884866+Arishba-Khan@users.noreply.github.com>
2024-04-20 22:52:04 +0500

    Delete CLI Number-guessing-game directory

--- CLI Number-guessing-game/app.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var random_number = Math.random();
-console.log(random_number);
--- CLI Number-guessing-game/app.ts
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-
-import inquirer from "inquirer";
-
-const random_number = Math.random();
-
-console.log(random_number);
-
-export{}
